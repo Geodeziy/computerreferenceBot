4201b377e8305df00b1a147f44d4dcf674d14b19
Geodez_y <geodez_y@bk.ru>
2024-04-01 09:48:08 +0300

    Исправление ошибок.

--- handlers/start.py
+++ handlers/start.py
@@ -1,8 +1,7 @@
 from aiogram import Bot, Dispatcher, types, html, F, Router
 from aiogram.filters import Command, CommandObject
-from aiogram.types import Message, WebAppInfo, WebAppData,  MenuButtonWebApp, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import Message, WebAppInfo, WebAppData, MenuButtonWebApp, InlineKeyboardMarkup, InlineKeyboardButton
 from filters.chat_type import ChatTypeFilter
-
 
 router = Router()
 router.message.filter(
@@ -16,5 +15,7 @@ async def command_start(message: Message, bot: Bot):
     #     chat_id=message.chat.id,
     #     menu_button=MenuButtonWebApp(text="Web", web_app=WebAppInfo(url="https://geodeziy.github.io/W-0723/templates/index.html")),
     # )
-    await message.answer("""Здравствуйте. Это бот с веб-справочником. Чтобы открыть веб-страницу справочника наберите эту команду /webview и нажмите
-     на кнопку под отправленным сообщением. Для дополнительных команд наберите команду /help.""")
+    await message.answer(
+        """Здравствуйте. Это бот с веб-справочником.
+Чтобы открыть веб-страницу справочника наберите эту команду /webview и нажмите на кнопку под отправленным сообщением.
+Для дополнительных команд наберите команду /help.""")
